3a7212128ba5ad1b0709b3d93063819b2537d5ce
Janez Žemva <janezz55@gmail.com>
2022-05-20 11:17:45 +0200

    some fixes

--- corodemo.cpp
+++ corodemo.cpp
@@ -26,23 +26,35 @@ int main()
     )
   );
 
-  cr2::basic::make_and_run<128_k, 128_k>(
-    [&](auto& c)
-    {
-      A a;
-
-      for (int i{}; i != 3; ++i)
+  std::cout << std::get<1>(
+    cr2::basic::make_and_run<128_k, 128_k>(
+      [&](auto& c)
       {
-        std::cout << i << '\n';
+        A a;
 
-        c.suspend_to(c0);
+        for (int i{}; i != 3; ++i)
+        {
+          std::cout << i << '\n';
+
+          c.suspend_to(c0);
+        }
+      },
+      [](auto& c)
+      {
+        std::intmax_t j(5);
+
+        for (auto i(j - 1); 1 != i; --i)
+        {
+          std::cout << "coro2\n";
+
+          j *= i;
+          c.suspend();
+        }
+
+        return j;
       }
-    },
-    [](auto& c)
-    {
-      std::cout << "coro2\n";
-    }
-  );
+    )
+  ) << '\n';
 
   return 0;
 }
